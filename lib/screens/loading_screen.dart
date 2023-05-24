5e28b70d4a85fcf7c2589b5d535a5b3f7250f913
Kuwuamenu Kofi Gabriel <74659884+Carlysberg@users.noreply.github.com>
2023-05-24 09:28:00 -0700

    understood jsonDecode with dart:convert

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 const API_KEY = '1d85d42390f13a6ddd788f49b50799f3';
 
 class LoadingScreen extends StatefulWidget {
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
 
   void getLocation()async{
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    longitude = location.longitude;
+    latitude = location.latitude;
+    print(latitude);
+    print(longitude);
   }
 
   void getData()async{
     var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${Location().latitude}&lon=${Location().longitude}&appid=${API_KEY}'
-
+        'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}'
     );
     var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
 
+    if(response.statusCode==200){
+
+    var temperature = jsonDecode(response.body)['main']['temp'];
+    var city = jsonDecode(response.body)['name'];
+    var condition = jsonDecode(response.body)['weather'][0]['id'];
+    print('temperature = $temperature');
+    print('city = $city');
+    print('condition = $condition');
+
+    }else{
+      print(response.statusCode);
+    }
   }
-
 
   @override
   void initState() {
     super.initState();
     getLocation();
-    getData();
+
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
 
     );
